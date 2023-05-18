620236d194634c10240b82f52461b9eb7579f026
8ctopus <hello@octopuslabs.io>
2023-05-18 14:31:16 +0400

    Fix route exception class was missing Add route exception class tests

---
+++ src/RouteException.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oct8pus\NanoRouter;
+
+use Exception;
+
+class RouteException extends Exception
+{
+    public function __construct(string $message, int $code)
+    {
+        if ($code < 100 || $code >= 600) {
+            throw new NanoRouterException("invalid status code - {$code}");
+        }
+
+        parent::__construct($message, $code);
+    }
+}
---
+++ tests/RouteExceptionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use HttpSoft\Message\Response;
+use HttpSoft\Message\Stream;
+use Oct8pus\NanoRouter\NanoRouter;
+use Oct8pus\NanoRouter\NanoRouterException;
+use Oct8pus\NanoRouter\RouteException;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @internal
+ *
+ * @covers \Oct8pus\NanoRouter\RouteException
+ */
+final class RouteExceptionTest extends TestCase
+{
+    public function testOK() : void
+    {
+        $exception = new RouteException('hello', 403);
+
+        static::assertSame('hello', $exception->getMessage());
+        static::assertSame(403, $exception->getCode());
+    }
+
+    public function testInvalidStatusCode() : void
+    {
+        static::expectException(NanoRouterException::class);
+        static::expectExceptionMessage('invalid status code - 99');
+
+        new RouteException('test', 99);
+    }
+
+    public function testInvalidStatusCode2() : void
+    {
+        static::expectException(NanoRouterException::class);
+        static::expectExceptionMessage('invalid status code - 600');
+
+        new RouteException('test', 600);
+    }
+}
